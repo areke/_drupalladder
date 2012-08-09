d59f9766b3f5b28d5eb86ad7ad6af3ceda9300df
Bryan Hirsch <bryan.hirsch@acquia.com>
2012-08-09 01:31:29 -0400

    Enable error reporting in dev branch setting.php.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -535,3 +535,8 @@ if (file_exists('/var/www/site-php')) {
 }
 //$conf['profile'] = 'drupalladder';
 $conf['install_profile'] = 'drupalladder';
+
+error_reporting(-1);  // Have PHP complain about absolutely everything.
+$conf['error_level'] = 2;  // Show all messages on your screen
+ini_set('display_errors', TRUE);  // These lines give you content on WSOD pages.
+ini_set('display_startup_errors', TRUE);
